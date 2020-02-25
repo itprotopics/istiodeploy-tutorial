9ce89e7405b8440e99fd5bd133c2d172848ee4d9
JLAM <jose.alavez@hotmail.com>
2020-02-25 11:33:17 -0600

    Se realizan cambios para compilacion exitosa

--- mspmc-capauno/src/main/java/mx/gob/imss/cit/mspmccapauno/MsCapaUnoApplication.java
+++ mspmc-capauno/src/main/java/mx/gob/imss/cit/mspmccapauno/MsCapaUnoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.samplers.ProbabilisticSampler;
+import io.opentracing.Tracer;
+
 @SpringBootApplication
 public class MsCapaUnoApplication {
 
@@ -18,4 +22,15 @@ public class MsCapaUnoApplication {
         return restTemplateBuilder.build();
     }
 
+    @Bean
+	public Tracer getTracer() {
+		Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv()
+				.withType(ProbabilisticSampler.TYPE).withParam(1);
+		Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
+				.withLogSpans(true);
+		Configuration config = new Configuration("mspmccapauno Service").withSampler(samplerConfig)
+				.withReporter(reporterConfig);
+		return config.getTracer();
+	}
+    
 }
--- mspmc-capauno/src/main/java/mx/gob/imss/cit/mspmccapauno/controller/CapaUnoController.java
+++ mspmc-capauno/src/main/java/mx/gob/imss/cit/mspmccapauno/controller/CapaUnoController.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.opentracing.Tracer;
 import mx.gob.imss.cit.mspmccapauno.exceptions.BusinessException;
+import mx.gob.imss.cit.mspmccapauno.integration.enums.EnumHttpStatus;
 import mx.gob.imss.cit.mspmccapauno.integration.model.CapaUnoModel;
 import mx.gob.imss.cit.mspmccapauno.integration.model.ErrorResponse;
 import mx.gob.imss.cit.mspmccapauno.services.CapaUnoService;
@@ -32,24 +35,33 @@ public class CapaUnoController {
     @ResponseStatus(HttpStatus.OK)
     public void live() {}
     
-//	@Bean
-//	public io.opentracing.Tracer tracer() {
-//		return Configuration.fromEnv().getTracer();
-//	}
-//	
+    @Autowired
+    private Tracer tracer;
+	
     @RequestMapping("/recursoscapauno")
-    public Object getRecursoCapaUno() {
+    public Object getRecursoCapaUno(@RequestHeader("User-Agent") String userAgent, 
+    		@RequestHeader(value = "user-preference", required = false) String userPreference) {
     	
     	Object respuesta = null;
     	
         count++;
-        
-
         logger.debug("mspmccapauno service ready to return");
         
         CapaUnoModel model;
         
         try {
+        	
+        	try {
+	        	tracer.activeSpan().setBaggageItem("user-agent", userAgent);
+	            if (userPreference != null && !userPreference.isEmpty()) {
+	                tracer.activeSpan().setBaggageItem("user-preference", userPreference);
+	            }
+        	}
+        	catch (Exception ex) {
+        		throw new BusinessException(EnumHttpStatus.SERVER_ERROR_INTERNAL, "Este es un mensaje de error de negocio",
+    					ex.getMessage());
+        	}
+            
 	        model = capaUnoService.capaUnoMethod();
 	        model.setCount(count);
 	
@@ -65,6 +77,7 @@ public class CapaUnoController {
             respuesta = new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.valueOf(numberHTTPDesired));
  
         }
+        
         
         return respuesta;
     }
